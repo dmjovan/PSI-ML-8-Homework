2335647ac49fc1d47b90b858cec639fd8090fc1b
Mihailo Grbic <mihailogrbic98@gmail.com>
2022-03-14 02:01:03 +0100

    add: C

--- TaskB/main.py
+++ TaskB/main.py
@@ -33,7 +33,6 @@ unique_json_counter = 0
 
 all_att, all_att_num = 0, 0
 
-
 ##################### IMPLEMENTATION ####################
 
 
@@ -47,28 +46,28 @@ def parse_year(year: str) -> str:
                 year = "200" + str(year)
             else:
                 year = "20" + str(year)
-    return int(year)
+    return str(year)
 
 
 def parse_month(month: str) -> str:
-    return int(month)
+    return str(int(month))
 
 
 def parse_day(day: str) -> str:
-    return int(day)
+    return str(int(day))
 
 
 def parse_country(country: str) -> str:
 
-    # # Remove prefix
-    # PREFIX = 'the_'
-    # if country[:len(PREFIX)] == PREFIX:
-    #     country = country[len(PREFIX):]
-    
     # Replace dashs with underscore
     country = country.replace('-','_')
 
-    return country.strip('the_')
+    # Remove prefix
+    PREFIX = 'the_'
+    if country[:len(PREFIX)] == PREFIX:
+        country = country[len(PREFIX):]
+
+    return country#.strip('the_')
 
 
 def parse_city(city: str) -> str:
@@ -87,7 +86,7 @@ def parse_json(json_path: str) -> list:
 
     def parse_event(event: dict) -> list:
         """ Parsing event dict """
-        nonlocal att, num_att, json_name, json_path
+        nonlocal att, num_att, json_name, year, month, day, country, city
 
         # Keys of dict
         event_keys = event.keys()
@@ -100,8 +99,8 @@ def parse_json(json_path: str) -> list:
             
             # Handilng 'attendance'
             if "attendance" not in event_keys:
-                event["attendance"] = str(json_path)
-            else: 
+                event["attendance"] = ('_').join([json_name, country, city, year, month, day])
+            else:
                 att += event["attendance"]
                 num_att += 1
 
@@ -141,6 +140,11 @@ def parse_json(json_path: str) -> list:
     # Get json name (for DataFrame)
     json_name = str(json_path.stem).replace('-','_')
 
+    # # Remove prefix
+    # PREFIX = 'the_'
+    # if json_name[:len(PREFIX)] == PREFIX:
+    #     json_name = json_name[len(PREFIX):]
+
     # Extract info from path
     year, month, day, country, city = pattern_matching(str(json_path))
 
@@ -155,19 +159,19 @@ def parse_json(json_path: str) -> list:
         venue_stats["band_name"].append(MISSING)
         venue_stats["is_indie"].append(MISSING)
         venue_stats["attendance"].append(MISSING)
-        venue_stats["invalid"].append(MISSING)
+        venue_stats["invalid"].append(True)
     else:
         [parse_event(event) for event in venue]
 
     if num_att > 0:
         all_att += att
         all_att_num += num_att
-        att /= num_att
+        att //= num_att
     else:
         att = MISSING
 
     # Add mapper
-    fill_attendance_map[str(json_path)] = att
+    fill_attendance_map[('_').join([json_name, country, city, year, month, day])] = att
 
 
 def pattern_matching(rel_path: str):
@@ -200,7 +204,6 @@ def pattern_matching(rel_path: str):
             city = parse_city(dirs[2])
 
     return year, month, day, country, city
-
 
 
 def main(set_path: str):
@@ -241,13 +244,28 @@ def main(set_path: str):
     # Extract only valid concerts
     stats_df = stats_df[stats_df.invalid == False]
 
+
+    # anoint_rat_idxs = stats_df[stats_df.band_name == "anoint_rat"].index.tolist()[:20]
+    #print(stats_df[stats_df.index.isin(anoint_rat_idxs)])
+
     # Fill attendance by mean of attendance by venue mean
     stats_df['attendance'] = stats_df['attendance'].apply(lambda x: x if x not in fill_attendance_map.keys() else fill_attendance_map[x])
-    
+
+    #print(stats_df[stats_df.index.isin(anoint_rat_idxs)])
+    # print(sorted(stats_df.band_name.unique().tolist()))
+    # print(sorted(stats_df.json_name.unique().tolist()))
+    # print(sorted(stats_df.year.unique().tolist()))
+    # print(sorted(stats_df.month.unique().tolist()))
+    # print(sorted(stats_df.day.unique().tolist()))
+    # print(sorted(stats_df.country.unique().tolist()))
+    # print(sorted(stats_df.city.unique().tolist()))
+
     # Fill attendance by mean of attendance by all mean # BUG
     stats_df['attendance'] = stats_df['attendance'].fillna(all_att)
-    #stats_df['attendance'] = stats_df['attendance'].fillna(stats_df['attendance'].mean())
+    # stats_df['attendance'] = stats_df['attendance'].fillna(stats_df['attendance'].mean()) 
 
+    #print("\nGlobal average", all_att)
+    #print(stats_df[stats_df.index.isin(anoint_rat_idxs)])
 
     ########################### ANSWER A ##################################
     A_ans = unique_json_counter
@@ -259,11 +277,14 @@ def main(set_path: str):
     print(B_ans)
 
     ########################### ANSWER C ##################################
-    C_ans = stats_df.city.value_counts().idxmax()
+    city_counts = stats_df.city.value_counts()
+    city_counts_max = city_counts.max()
+    C_ans = sorted(list(filter(lambda x: city_counts[x] == city_counts_max, city_counts.index.tolist())))[0]
     print(C_ans)
 
     ########################### ANSWER D ##################################
-    # print(stats_df[stats_df.is_indie == True].groupby('city').agg({"attendance": ['sum']}).sort_values(('attendance', 'sum'), ascending=False))
+    # stats_df['country_city'] = stats_df['country'] + '-8-' + stats_df['city']
+    # D_ans = (',').join(list(map(lambda x: x.split('-8-')[-1], stats_df[stats_df.is_indie == True].groupby('country_city').agg({"attendance": ['sum']}).sort_values(('attendance', 'sum'), ascending=False).index[:3])))
     D_ans = (',').join(stats_df[stats_df.is_indie == True].groupby('city').agg({"attendance": ['sum']}).sort_values(('attendance', 'sum'), ascending=False).index[:3])
     print(D_ans)
 
@@ -276,9 +297,9 @@ def main(set_path: str):
     concert_info_with_indie = stats_df[stats_df.is_indie == True].concert_info.unique()
 
     # Most popular bands based on average attendance 
+    # print(stats_df[stats_df.concert_info.isin(concert_info_with_indie)].groupby('band_name').agg({"attendance": ['mean']}).sort_values(('attendance', 'mean'), ascending=False))
     E_ans = (',').join(stats_df[stats_df.concert_info.isin(concert_info_with_indie)].groupby('band_name').agg({"attendance": ['mean']}).sort_values(('attendance', 'mean'), ascending=False).index[:3])
     print(E_ans)
-
 
     return [A_ans, B_ans, C_ans, D_ans, E_ans]
 
@@ -288,30 +309,31 @@ def main(set_path: str):
 if __name__ == "__main__":
 
     # Get input
-    set_path = input() # BUG
-    #set_path = "/home/grbic/Desktop/PSIML:8/B/public/sets/8"
+    # set_path = input().strip('\n') # BUG
+    
+    # set_path = "/home/grbic/Desktop/PSI-ML-8-Homework/TaskB/test_dataset"
+    # set_path = "/home/grbic/Desktop/PSIML:8/B/public/sets/6"
+    # ans = main(set_path)
 
-    ans = main(set_path)
 
+    # Public test
+    for i in range(10):
+        print(f"\n\n#################### {i+1} ################")
 
-    # # Public test
-    # for i in range(10):
-    #     print(f"\n\n#################### {i+1} ################")
+        # Path to dataset
+        set_path = f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskB/dataset/public/sets/{i}"
 
-    #     # Path to dataset
-    #     set_path = f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskB/dataset/public/sets/{i}"
+        # Run script
+        s = time.time()
+        ans = main(set_path)
+        print(f"Elapsed time: {round(time.time()-s,2)}s\n")
 
-    #     # Run script
-    #     s = time.time()
-    #     ans = main(set_path)
-    #     print(f"Elapsed time: {round(time.time()-s,2)}s\n")
+        # Read expected output
+        with open(f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskB/dataset/public/outputs/case-{i+1}.out") as f:
+            out = list(map(lambda x: x.strip('\n'), f.readlines()))
 
-    #     # Read expected output
-    #     with open(f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskB/dataset/public/outputs/case-{i+1}.out") as f:
-    #         out = list(map(lambda x: x.strip('\n'), f.readlines()))
-
-    #     for a, o, l in zip(ans, out, ['A','B','C','D','E']):
-    #         print(f"\n------- {l} -------\n")
-    #         print(a)
-    #         print(o)
-
+        for a, o, l in zip(ans, out, ['A','B','C','D','E']):
+            if str(a) != o:
+                print(f"\n------- {l} -------\n")
+                print("Output:", a)
+                print("Expected:", o)
--- TaskC/main.py
+++ TaskC/main.py
@@ -1,31 +1,41 @@
+from copy import deepcopy
 import os
-import time
 import numpy as np
 from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
 
 class Sudoku:
 
     def __init__(self, image_path: str) -> None:
 
-        self.image_path = image_path
+        self.image_path = os.path.join(image_path, image_path.split(os.path.sep)[-1]) + ".png"
         self.sudoku_image = None
         self.tile_width = None
         self.skip_width = None
         self.skip_width3 = None
         self.digits = {}
         self.rotation_cnts = None
+        self.rot_votes = {0: 0,
+                          1: 0,
+                          2: 0,
+                          3: 0}
         self.table = np.zeros((9,9), dtype=int)
 
         self.load_sudoku_image()
         self.load_digits()
         self.parse_tiles()
         self.rotate()
+        
+        self.table_original = deepcopy(self.table)
 
     def __repr__(self) -> str:
         rep = ""
         for i in range(9):
-            rep += str(list(self.table[i]))[1:-1] + "\n"
-
+            rep += (str(list(self.table_original[i]))[1:-1]).replace(" ", "") + "\n" 
+        
+        for i in range(9):
+            rep += (str(list(self.table[i]))[1:-1]).replace(" ", "") + ("\n" if i < 8 else '')
+        
         return rep
 
     def load_sudoku_image(self) -> None:
@@ -34,7 +44,8 @@ class Sudoku:
         image_file = ImageOps.grayscale(image_file)
         image = np.array(image_file)
 
-        image[image > 0] = 255
+        image[image >= 128] = 255
+        image[image < 128] = 0
 
         height, width = image.shape
 
@@ -61,25 +72,54 @@ class Sudoku:
         self.skip_width = int(t - np.sum(self.sudoku_image[0,:t])/255)
         self.skip_width3 = int((self.sudoku_image.shape[1] - 6*self.skip_width - 9*self.tile_width + 1)/2)
 
+    
+    def get_digit(self, num, shape = tuple) -> np.ndarray:
+
+        image_file = self.digits[num]
+
+        image_npy = np.array(image_file)
+        image_npy = 255 - image_npy
+        sum_by_col = np.sum(image_npy, axis=1)
+        idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+        start_r, end_r = idxs[0], idxs[-1]
+        image_npy = image_npy[start_r:(end_r+1),:]
+
+        sum_by_col = np.sum(image_npy, axis=0)
+        idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+        start_c, end_c = idxs[0], idxs[-1]
+        image_npy = image_npy[:, start_c:(end_c+1)]
+
+
+        image_file = Image.fromarray(image_npy)
+
+        #s = time.time()
+        image_file = image_file.resize(shape)
+        #print(time.time() - s)
+        digit = np.array(image_file)
+
+        digit[digit >= 128] = 255
+        digit[digit < 128] = 0
+
+        return digit
+
     def load_digits(self) -> None:
 
         dir_name = os.path.dirname(self.image_path)
         for i in range(1,10):
-            image_file = Image.open(dir_name + "/digits/" + str(i) + ".png")
-            image_resized = image_file.resize((self.tile_width, self.tile_width))
-            image = np.array(image_resized)
+            image_file = Image.open(os.path.join(dir_name, "digits", str(i) + ".png"))
+            image = np.array(image_file)
             image = 255 - image[:, :, 3]
 
             img = Image.fromarray(image, 'L')
             image_file = ImageOps.grayscale(img)
-            digit_img = np.array(image_file)
+            image_file = image_file.resize((self.tile_width, self.tile_width))
 
-            digit = 255 - digit_img
-            digit[digit > 0] = 255
+            self.digits[i] = image_file
 
-            self.digits[i] = digit
 
     def parse_tiles(self):
+
+        self.tile_number_dict = {}
 
         for i in range(81):
             row = i // 9
@@ -96,23 +136,74 @@ class Sudoku:
 
             tile = self.sudoku_image[vertical_start: vertical_start + self.tile_width, horizontal_start: horizontal_start + self.tile_width]
             tile_number = self.get_number_from_tile(tile)
-            
-            self.add(tile_number, (row, col))
+
+            if isinstance(tile_number, int):
+                self.add(tile_number, (row, col))
+            else:
+                self.tile_number_dict[i] = tile_number
 
         
     def get_number_from_tile(self, tile: np.ndarray) -> int:
 
-        H,W = tile.shape
-
         def test_number(tile: np.ndarray, num: int, k: int=0) -> int:
             if k == 0:
-                digit_product = tile * self.digits[num][0:H, 0:W]
+                # crop only digit
+                sum_by_col = np.sum(tile, axis=1)
+                idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+                start_r, end_r = idxs[0], idxs[-1]
+                tile = tile[start_r:(end_r+1),:]
+
+                sum_by_col = np.sum(tile, axis=0)
+                idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+                start_c, end_c = idxs[0], idxs[-1]
+                tile = tile[:, start_c:(end_c+1)]
+
+                H,W = tile.shape
+                
+                #s = time.time()
+                digits_transformed = self.get_digit(num, (W,H))
+                #print(time.time() - s)
+                
+                plt.figure()
+                plt.imshow(tile)
+                plt.show()
+                plt.figure()
+                plt.imshow(digits_transformed)
+                plt.show()
+
+                
+                digit_product = (tile  == digits_transformed).sum()
+                #print(time.time() - s)
+      
             else:
-                digit_product = tile * np.rot90(self.digits[num][0:H, 0:W], k=k, axes=(0,1)).reshape((H,W))
 
-            digit_product[digit_product > 0] = 255
+                # crop only digit
+                sum_by_col = np.sum(tile, axis=1)
+                idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+                start_r, end_r = idxs[0], idxs[-1]
+                tile = tile[start_r:(end_r+1),:]
 
-            return np.sum(digit_product) / 255
+                sum_by_col = np.sum(tile, axis=0)
+                idxs = [i for i in range(sum_by_col.size) if sum_by_col[i] > 0]
+                start_c, end_c = idxs[0], idxs[-1]
+                tile = tile[:, start_c:(end_c+1)]
+
+                tile_transformed = np.rot90(tile, k=k, axes=(0,1))
+                H,W = tile_transformed.shape
+
+                digits_transformed = self.get_digit(num, (W,H))
+
+                plt.figure()
+                plt.imshow(tile_transformed)
+                plt.show()
+                plt.figure()
+                plt.imshow(digits_transformed)
+                plt.show()
+
+                digit_product = (tile_transformed  == digits_transformed).sum()
+            print(digit_product)
+
+            return digit_product
 
 
         if np.sum(tile) / 255 == tile.size : # empty tile
@@ -120,30 +211,47 @@ class Sudoku:
 
         tile = 255 - tile
 
+        number_per_rot = {}
+
         if self.rotation_cnts is None:
-            lst  = []
-            for k in range(4):
-                lst.append([test_number(tile, num, k) for num in range(1,10)])
+            test_rotation = np.zeros((4,9), dtype=int)
             
-            test_rotation = np.array(lst)
+            for k in range(4):
+                test_rotation[k, :] = np.array([test_number(tile, num, k) for num in range(1,10)])
+                number_per_rot[k] = np.argmax(test_rotation[k, :]) + 1
+                
+            # k_opt1 = np.argwhere(test_rotation - )
+
             k_opt, number = np.unravel_index(np.argmax(test_rotation, axis=None), test_rotation.shape)
 
-            self.rotation_cnts = k_opt
+            # self.rotation_cnts = k_opt
+            self.rot_votes[k_opt] += 1
 
-            number += 1
+            # number += 1
 
-        else:
-            white_pixels = np.array([test_number(tile, num, self.rotation_cnts) for num in range(1,10)])
-            number = np.argmax(white_pixels) + 1
 
-        return number
+        return number_per_rot
 
     def rotate(self) -> None:
 
-        print('rotation', self.rotation_cnts)
+        if self.rotation_cnts is None:
+            self.rotation_cnts = max(self.rot_votes, key=self.rot_votes.get)
+            if self.rot_votes[1] + self.rot_votes[3] > 0:
+                if self.rot_votes[1] > self.rot_votes[3]:
+                    self.rotation_cnts = 1
+                else:
+                    self.rotation_cnts = 3
+            print(self.rotation_cnts)
+                    
+            for i in range(81):
+                if i in self.tile_number_dict.keys():
+                    row = i // 9
+                    col = i % 9
+                    # print(row, col, self.tile_number_dict[i])
+                    self.add(self.tile_number_dict[i][self.rotation_cnts], (row, col))
 
         if self.rotation_cnts > 0:
-            self.table = np.rot90(self.table, k=self.rotation_cnts, axes=(0,1))
+            self.table = np.rot90(self.table, k=4-self.rotation_cnts, axes=(0,1))
 
     def add(self, number: int, position: tuple) -> None:
         self.table[position] = number
@@ -190,25 +298,21 @@ class Sudoku:
 
         return False
 
-
 if __name__ == "__main__":
 
-    # path = input()
-
-    for i in range(10):
-
-        path = r"TaskC\dataset\public\set\0" + str(i) + r"\0" + str(i) + ".png"
-        print(path)
-
-        start = time.time()
-
+    # path = input().strip("\n")
+    for t in range(5, 6):
+        print('=========================================')
+        tt = "0" + str(t)
+        path = f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskC/dataset/public/set/{tt}"
+        #import time
+        #start = time.time()
         sudoku = Sudoku(path)
-
+        sudoku.solve()
         print(sudoku)
-        # sudoku.solve()
-        # print(sudoku)
-        
-        print(time.time() - start)
-        print('-------------------------------------------')
 
-        del sudoku
+        with open(f"/home/grbic/Desktop/PSI-ML-8-Homework/TaskC/dataset/public/outputs/{tt}.txt", 'r') as f:
+            expected = ('').join(f.readlines())
+
+        print('------------------------------')
+        print(expected)
